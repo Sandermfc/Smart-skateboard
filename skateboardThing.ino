77b913cbcbe1f55b27f80f2a9c58b3cf329973d0
jmlevesque333 <jmlevesque333@gmail.com>
2017-01-07 14:56:43 -0400

    Update skateboardThing.ino

--- skateboardThing.ino
+++ skateboardThing.ino
@@ -1,12 +1,12 @@
 #include "pullDataXG.h"
 #include <math.h>
 
-int offSetPitch; // starting point for yaw pitch roll
-int offSetYaw;
-int offSetRoll;
-float offSetX;
-float offSetY;
-float offSetZ;
+int offSetPitch=0; // starting point for yaw pitch roll
+int offSetYaw=0;
+int offSetRoll=0;
+float offSetX=0;
+float offSetY=0;
+float offSetZ=0;
 int counter = 0; // do functions this number of times unti give true
 
 //Baseline orientation (not the same as offset, which is to calibrate at a universal 0 point)
@@ -15,14 +15,14 @@ int baseYaw=0;
 int basePitch=0;
 int baseRoll=0;
 
-int prevHeight = 0;
+float prevHeight = 0;
 float prevVelocity = 0;
 unsigned long prevTime = 0;
 acceleration prevAccel;
 
 float maxHeight = 0;
 double currentHeight = 0;
-int frontLiftAngle = 40;
+const int frontLiftAngle = 35;
 
 //pullDataXG.h deals with the accelerometer/gyro pins
 float calculateVelocity();
@@ -35,36 +35,47 @@ class state
   float velocity; //velocity (speed) in the y  (upward) axis only
   unsigned long t;
   
-  void getData()
+  bool getData()
   {
     //get the current time
     //Read the orientation and accelerations on xyz axes
     allData myData = pullStuff(); //yaw, pitch, roll, xyz accelerations TODOOOO
+    if(myData.o.yaw == 0 && myData.o.pitch == 0 && myData.o.roll == 0 && myData.a.x == 0 && myData.a.y == 0 && myData.a.z == 0)
+    {
+      Serial.print(F("got an error, returning false"));
+      return false;
+    }
     myOrientation = myData.o;
     myAcceleration = myData.a;
-    
     //myAcceleration.x-=offSetX;
     //myAcceleration.y-=offSetY;
-    //myAcceleration.z-=offSetZ;
-    
     //map acceleration to 2G interval (-19.6 to +19.6)
     myAcceleration.x/= 1671.8367346939;
     myAcceleration.y/= 1671.8367346939;
     myAcceleration.z/= 1671.8367346939;
-    //myAcceleration.x = map(myAcceleration.x, -32768, 32768, -19.6, 19.6);
-    //myAcceleration.y = map(myAcceleration.y, -32768, 32768, -19.6, 19.6);
-    //myAcceleration.z = map(myAcceleration.z, -32768, 32768, -19.6, 19.6);
-    Serial.print("x ");
-    Serial.println(myAcceleration.x);
-    Serial.print("y ");
-    Serial.println(myAcceleration.y);
-    Serial.print("z ");
+
+
+    //Serial.print(F("myAcceleration.z = "));
+    //Serial.println(myAcceleration.z);
+    //Serial.print(F("After offset = "));
+    myAcceleration.z-=offSetZ;
+    Serial.print(F("Zaccel = "));
     Serial.println(myAcceleration.z);
-   /* Serial.print("yaw ");
-    Serial.println(myOrientation.yaw);
+    //Serial.println(myAcceleration.z);
+    //Serial.print(F("With an offset of "));
+    //Serial.print(offSetZ);
+
+    //Serial.print("x ");
+    //Serial.println(myAcceleration.x);
+    //Serial.print("y ");
+    //Serial.println(myAcceleration.y);
+    //Serial.print("z ");
+    //Serial.println(myAcceleration.z);
+    //Serial.print("yaw ");
+    //Serial.println(myOrientation.yaw);
     Serial.print("pitch ");
     Serial.println(myOrientation.pitch);
-    Serial.print("roll ");
+    /*Serial.print("roll ");
     Serial.println(myOrientation.roll);
     Serial.print("x ");
     Serial.println(myAcceleration.x);
@@ -75,22 +86,21 @@ class state
 
     //at instant 0, previous speed is set to 0 by the initialise function
     //using previous speed, time between the last reading and this one, and the upward acceleration, find the current speed.
-    //speed = (prevSpeed
     //vitesseFinale = vitesseInitiale + acceleration*temps
-    currentHeight += addToHeight();
     t = micros();
-    Serial.println(t);
+    currentHeight += addToHeight();
+    //Serial.println(t);
     velocity = calculateVelocity();
     Serial.print("current Height ");
     Serial.println(currentHeight);
-    Serial.print("velociratpor ");
-    Serial.println(velocity);
+    /*Serial.print("velociratpor ");
+    Serial.println(velocity);*/
     
     //Set the prev**** variables for the next time around
     prevTime = t;
     prevVelocity = velocity;
     prevAccel = myAcceleration;
-    
+    return true;
   }
   /*int calculateHeight(int ultraSoundNum) // math function to calculate max height
   {
@@ -100,37 +110,56 @@ class state
 
 float calculateVelocity()
 {
-  Serial.println("CALCULATE VEL FUNC ");
-  if(data.myAcceleration.z < 0.67 && data.myAcceleration.z > 0.64)
+  //Serial.println("CALCULATE VEL FUNC ");
+  if((data.myAcceleration.z < 0.15 && data.myAcceleration.z > -0.15) && (data.myOrientation.pitch < 20 && data.myOrientation.pitch > -20))
   {
     Serial.println("still");
     return 0.0;
   }
   
   float x = prevVelocity;
-  Serial.print(F("Velocity1 "));
-  Serial.println(x);
+  //Serial.print(F("Velocity1 "));
+  //Serial.println(x);
   double fdeltaT = (data.t - prevTime)*0.000004;
-  Serial.print("t 1 ");
-  Serial.println(data.t);
-  Serial.print("t 2 ");
-  Serial.println(prevTime);
-  Serial.print("fdeltaT ");
-  Serial.println(fdeltaT);
+  //Serial.print("t 1 ");
+  //Serial.println(data.t);
+  //Serial.print("t 2 ");
+  //Serial.println(prevTime);
+  //Serial.print("fdeltaT ");
+  //Serial.println(fdeltaT);
   x += (data.myAcceleration.z)*fdeltaT; //TODO make sure its in seconds
-  Serial.print(F("Velocity2 "));
-  Serial.print(x);
+  //Serial.print(F("Velocity2 "));
+  //Serial.print(x);
   return x;
 }
 float addToHeight()
 {
   //calculate distance travelled over 1 tick, adds this value to currentHeight
   //distance = initialVelocity*time + 1/2(accelerationY)*time^2
+  
   unsigned long deltaT = data.t - prevTime;
-  double fdeltaT = deltaT * 0.000004; //Make sure its in seconds
+  /*Serial.print(data.t);
+  Serial.print(F("-"));
+  Serial.print(prevTime);
+  Serial.print(F(" = "));
+  Serial.print(deltaT);*/
+  float fdeltaT = deltaT * 0.000004; //Make sure its in seconds
+  //Serial.print(F("fdeltaT = "));
+  //Serial.println(fdeltaT);
   float accelerationAverage = (data.myAcceleration.z + prevAccel.z)/2; //average of previous acceleration and current one
   //accelerationAverage -= offSetY;
-  return prevVelocity*fdeltaT + 0.5*accelerationAverage*(fdeltaT*fdeltaT);
+  /*Serial.println(F("()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()"));
+  Serial.print(prevVelocity);
+  Serial.print(F("*"));
+  Serial.print(fdeltaT);
+  Serial.print(F(" + 0.5*"));
+  Serial.print(accelerationAverage);
+  Serial.print(F("*"));
+  Serial.print(fdeltaT);
+  Serial.print(F("^2 = "));*/
+  float answer = prevVelocity*fdeltaT + 0.5*accelerationAverage*(fdeltaT*fdeltaT);
+  //Serial.println(answer);
+  return answer;
 }
 
 typedef bool(*functor)(void);
@@ -139,6 +168,7 @@ bool initialise()
 {
   currentHeight = 0;
   maxHeight = 0;
+  prevHeight=0;
   //these values are substracted from the angle found
   //this makes it so that when we start on an incline, that point becomes (0,0,0) until the end of the trick.
   baseYaw = data.myOrientation.yaw; //frontOfTable-1 is the most recently read value.
@@ -151,7 +181,7 @@ bool initialise()
   for(int i=0; i<10; i++)
   {
     data.getData();
-    if(data.myAcceleration.y < 1 && data.myAcceleration.y > -1)
+    if(data.myAcceleration.z < 1 && data.myAcceleration.z > -1)
     {
       //we good
     }
@@ -168,34 +198,25 @@ bool frontLift()
   if(data.myOrientation.pitch > frontLiftAngle) //TODO
   {
     front = 0; //0 if angle is positive
+    Serial.println("00000000000000000000000000000000000000000000000000000000000");
     return true;
   }
-  else if(data.myOrientation.pitch < 360 - frontLiftAngle) //TODO
+  else if(data.myOrientation.pitch <  -1*frontLiftAngle) //TODO
   {
     front = 1; //1 if angle is negative
+    Serial.println("11111111111111111111111111111111111111111111111111111111111");
     return true;
   }
   return false;
 }
 bool backLift()
 {
-  if(!front)//if first to lift was 2 check abs height 1
+  if(currentHeight > 1)// TODO 
   {
-    if(data.myOrientation.pitch < 360 - frontLiftAngle) 
-    {
-      Serial.println(F("+++++++++++++++++++++++++++++++++++++++++++++++++++++++"));
-      return true;
-    }
+    Serial.println(F("+++++++++++++++++++++++++++++++++++++++++++++++++++++++"));
+    return true;
   }
-  else
-  {
-    if(data.myOrientation.pitch > frontLiftAngle)
-    {
-      Serial.println(F("+++++++++++++++++++++++++++++++++++++++++++++++++++++++"));
-      return true;
-    }
-  }
-  return false;
+ return false;
 }
 
 bool getMaxHeight()
@@ -203,6 +224,8 @@ bool getMaxHeight()
   if(prevHeight > currentHeight)
   {
     maxHeight = prevHeight;
+    Serial.print("hfhabd ");
+    Serial.println(maxHeight);
     return true;
   }
   else 
@@ -214,10 +237,10 @@ bool getMaxHeight()
 
 bool landed() 
 {
-  const float marginAccel = 30.0;
+  const float marginAccel = 0.15;
   const float marginAngleP = 10.0;
-  if(-1*marginAccel < data.myAcceleration.y < marginAccel && -1*marginAngleP < data.myOrientation.pitch < marginAngleP)
-  {
+  if((-1*marginAccel < data.myAcceleration.z  && data.myAcceleration.z < marginAccel) && (-1*marginAngleP < data.myOrientation.pitch) && (data.myOrientation.pitch < marginAngleP))
+  { 
     Serial.print("//////////////////////////////////////////////////////");
     delay(2000);//TODO
     return true;
@@ -280,7 +303,7 @@ node* curr;
 
 void setup() {
   //Baud and pin setup
-  Serial.begin(57600); //print to serial monitor
+  Serial.begin(115200); //print to serial monitor
   //Serial.println(F("1"));
   /*pinMode(trigPin1, OUTPUT);
   pinMode(echoPin1, INPUT);
@@ -311,14 +334,13 @@ void setup() {
   bothLifted->node1();
   bothLifted->node2(getMaxHeight);
 
-  /*node* descending = (node*) malloc (sizeof(*descending));
+  node* descending = (node*) malloc (sizeof(*descending));
   bothLifted->pushChild(descending);
   descending->node1();
-  descending->node2(landed);*/
+  descending->node2(landed);
   
   //Serial.println(F("prepXG"));
   prepareXG(); //calls the initialise function in the pullDataXG.h header
-  
   for(int i =0; i< 1000; i++) // do it a 100 times to stabilize the values
   {
     data.getData();
@@ -333,6 +355,8 @@ void setup() {
   offSetZ = data.myAcceleration.z;
   curr = root;
   prevTime = micros(); //important for the very first acceleration calculation, otherwise prevTime = 0 and deltaT = 200s, distance becomes a lot and messes with future readings for a while
+  prevVelocity = 0;
+  prevAccel.x = prevAccel.y = prevAccel.z = 0;
 }
 
 void loop() {
@@ -343,19 +367,22 @@ void loop() {
   int nextChild = curr->test();
   if(nextChild != -1)
   {
+    counter = 0;
     curr = curr->getChild(nextChild);
     
     if(curr == NULL)
     {
       curr = root;
     }
-    data.getData();
+    while(!data.getData()){Serial.print(F("Trying to get data again ..................................."));} //getData until there was not a buffer error
   }
   else
   {
     if(counter == 100) // do the functions 5 times unless they return true
     {
       counter = 0;
+      Serial.print(F("FAILED"));
+      //delay(2000);
       curr = root; //retourne au debut
     }
      else
